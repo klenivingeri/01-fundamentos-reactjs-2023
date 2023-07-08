e909c2beac8304542da63c570099db8404c03276
klenivingeri <libertti@live.com>
2023-07-08 13:50:19 -0300

    chore: Finalizando SideBar

---
+++ README.md
@@ -0,0 +1,12 @@
+# App feed
+
+### Projeto 
+Template de feed
+
+## Tecnologias
+### Sites:
+https://unsplash.com/pt-br
+https://figma.com
+
+### Packages
+Icones : https://unsplash.com/pt-br
--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,4 +1,5 @@
 import style from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
   return (
@@ -9,17 +10,16 @@ export function Sidebar() {
       />
 
       <div className={style.profile}>
-        <div className={style.photo}>
           <img
             src="https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
           />
-        </div>
-        <strong>Erick Klenviving</strong>
+        <strong>Erick Kleniving</strong>
         <span>Full Stack developer</span>
       </div>
 
       <footer>
         <a href="#">
+          <PencilLine size={20} />
           Editar seu perfil
         </a>
       </footer>
