14dc49ec6cf631ee641f0e7c8391aeb83fcff000
Erick Kleniving <libertti@live.com>
2023-08-21 23:20:58 -0300

    chore: created form

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Template de feed
 
 
 ## NEW
-#### Tag time
+### JS time
 ~~~javascript 
 //component/Post
 //Ex: É uma boa pratica e ajuda com a acessibilidade manter a data original
@@ -23,5 +23,28 @@ Template de feed
 <time title="11 de maio ás 8:13" dataTime="2023-05-11 08:13:30">Publicado  há 1h</time>
 ~~~
 
+### CSS 
+#### rezise e footer
+~~~css
+/*component/Post */
+textarea {
+    rezise: none; /* Não permite que o campo seja redimensionado */
+}
+
+footer {/* Ocultar elemento */
+    /* display: none; Evitar usar display none, pq ele não é bom para acessibilidade */
+    visibility: hidden;
+    max-height: 0;
+}
+~~~
+
+#### focus-within
+~~~css
+/* Ex: quando qualquer elemenoto dentro do commentForm for focado, quero que o elemento footer tenha a seguinte alteração */
+.commentForm:focus-within footer {
+    visibility:visible;
+    max-height: none; /* max-height possui padrão none;
+}
+~~~
 
 
--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -5,7 +5,7 @@ export function Post(props) {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src='https://i.pinimg.com/originals/cf/d0/e6/cfd0e65ab7aafae09ad428810da84609.jpg' />
+                    <img className={styles.avatar} height='60' src='https://i.pinimg.com/originals/cf/d0/e6/cfd0e65ab7aafae09ad428810da84609.jpg' />
                     <div className={styles.authorInfo} >
                     <strong>Jake o cachorro</strong>
                     <span>Aventureiro</span>
@@ -13,15 +13,26 @@ export function Post(props) {
                 </div> 
                 <time title="11 de maio ás 8:13" dataTime="2023-05-11 08:13:30">Publicado  há 1h</time>
             </header>
+
             <div className={styles.content}>
                 <p>Fala galera 👋</p>
                 <p>Acabei de subir um novo episódio da nova tempoderada de Adventure Time.</p>
-                <p>👉 <a href="">Marshall Lee</a></p>
+                <p>👉 <a href="!#">Marshall Lee</a></p>
                 <p>
-                    <a href="">#Nova aventura</a>{' '}
-                    <a href="">#partiu</a>
+                    <a href="!#">#Nova aventura</a>{' '}
+                    <a href="!#">#partiu</a>
                 </p>
             </div>
+
+            <form className={styles.commentForm} action='#'>
+                <strong>Deixe seu feedback</strong>
+                <textarea
+                    placeholder='Deixei seu comentário'
+                />
+                <footer>
+                    <button type='submit'>Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
